17b8c876c5b40709bab67a827ee5b28149056644
Justin Ridgewell <justin@ridgewell.name>
2013-03-04 23:51:13 -0500

    Initial Commit

---
+++ ActivityLoader/ActivityLoaderController.h
@@ -0,0 +1,23 @@
+//
+//  ActivityLoaderController.h
+//  ActivityLoader
+//
+//  Created by Justin Ridgewell on 3/4/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <Preferences/Preferences.h>
+
+@interface ActivityLoaderController : PSListController
+{
+}
+
+- (id)getValueForSpecifier:(PSSpecifier*)specifier;
+- (void)setValue:(id)value forSpecifier:(PSSpecifier*)specifier;
+- (void)followOnTwitter:(PSSpecifier*)specifier;
+- (void)visitWebSite:(PSSpecifier*)specifier;
+- (void)makeDonation:(PSSpecifier*)specifier;
+
+@end
